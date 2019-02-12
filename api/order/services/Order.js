818c5fb6490377c1dc6b163bd0b1781ae2aa5041
Amine Nokra <amine.nokra@gmail.com>
2019-02-12 11:47:25 +0100

    datamodels

--- api/order/services/Order.js
+++ api/order/services/Order.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Tag.js service
+ * Order.js service
  *
  * @description: A set of functions similar to controller's actions to avoid code duplication.
  */
@@ -12,21 +12,21 @@ const _ = require('lodash');
 module.exports = {
 
   /**
-   * Promise to fetch all tags.
+   * Promise to fetch all orders.
    *
    * @return {Promise}
    */
 
   fetchAll: (params) => {
     // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('tag', params);
+    const filters = strapi.utils.models.convertParams('order', params);
     // Select field to populate.
-    const populate = Tag.associations
+    const populate = Order.associations
       .filter(ast => ast.autoPopulate !== false)
       .map(ast => ast.alias)
       .join(' ');
 
-    return Tag
+    return Order
       .find()
       .where(filters.where)
       .sort(filters.sort)
@@ -36,90 +36,90 @@ module.exports = {
   },
 
   /**
-   * Promise to fetch a/an tag.
+   * Promise to fetch a/an order.
    *
    * @return {Promise}
    */
 
   fetch: (params) => {
     // Select field to populate.
-    const populate = Tag.associations
+    const populate = Order.associations
       .filter(ast => ast.autoPopulate !== false)
       .map(ast => ast.alias)
       .join(' ');
 
-    return Tag
-      .findOne(_.pick(params, _.keys(Tag.schema.paths)))
+    return Order
+      .findOne(_.pick(params, _.keys(Order.schema.paths)))
       .populate(populate);
   },
 
   /**
-   * Promise to count tags.
+   * Promise to count orders.
    *
    * @return {Promise}
    */
 
   count: (params) => {
     // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('tag', params);
+    const filters = strapi.utils.models.convertParams('order', params);
 
-    return Tag
+    return Order
       .count()
       .where(filters.where);
   },
 
   /**
-   * Promise to add a/an tag.
+   * Promise to add a/an order.
    *
    * @return {Promise}
    */
 
   add: async (values) => {
     // Extract values related to relational data.
-    const relations = _.pick(values, Tag.associations.map(ast => ast.alias));
-    const data = _.omit(values, Tag.associations.map(ast => ast.alias));
+    const relations = _.pick(values, Order.associations.map(ast => ast.alias));
+    const data = _.omit(values, Order.associations.map(ast => ast.alias));
 
     // Create entry with no-relational data.
-    const entry = await Tag.create(data);
+    const entry = await Order.create(data);
 
     // Create relational data and return the entry.
-    return Tag.updateRelations({ _id: entry.id, values: relations });
+    return Order.updateRelations({ _id: entry.id, values: relations });
   },
 
   /**
-   * Promise to edit a/an tag.
+   * Promise to edit a/an order.
    *
    * @return {Promise}
    */
 
   edit: async (params, values) => {
     // Extract values related to relational data.
-    const relations = _.pick(values, Tag.associations.map(a => a.alias));
-    const data = _.omit(values, Tag.associations.map(a => a.alias));
+    const relations = _.pick(values, Order.associations.map(a => a.alias));
+    const data = _.omit(values, Order.associations.map(a => a.alias));
 
     // Update entry with no-relational data.
-    const entry = await Tag.update(params, data, { multi: true });
+    const entry = await Order.update(params, data, { multi: true });
 
     // Update relational data and return the entry.
-    return Tag.updateRelations(Object.assign(params, { values: relations }));
+    return Order.updateRelations(Object.assign(params, { values: relations }));
   },
 
   /**
-   * Promise to remove a/an tag.
+   * Promise to remove a/an order.
    *
    * @return {Promise}
    */
 
   remove: async params => {
     // Select field to populate.
-    const populate = Tag.associations
+    const populate = Order.associations
       .filter(ast => ast.autoPopulate !== false)
       .map(ast => ast.alias)
       .join(' ');
 
     // Note: To get the full response of Mongo, use the `remove()` method
     // or add spent the parameter `{ passRawResult: true }` as second argument.
-    const data = await Tag
+    const data = await Order
       .findOneAndRemove(params, {})
       .populate(populate);
 
@@ -128,7 +128,7 @@ module.exports = {
     }
 
     await Promise.all(
-      Tag.associations.map(async association => {
+      Order.associations.map(async association => {
         if (!association.via || !data._id) {
           return true;
         }
@@ -149,22 +149,22 @@ module.exports = {
   },
 
   /**
-   * Promise to search a/an tag.
+   * Promise to search a/an order.
    *
    * @return {Promise}
    */
 
   search: async (params) => {
     // Convert `params` object to filters compatible with Mongo.
-    const filters = strapi.utils.models.convertParams('tag', params);
+    const filters = strapi.utils.models.convertParams('order', params);
     // Select field to populate.
-    const populate = Tag.associations
+    const populate = Order.associations
       .filter(ast => ast.autoPopulate !== false)
       .map(ast => ast.alias)
       .join(' ');
 
-    const $or = Object.keys(Tag.attributes).reduce((acc, curr) => {
-      switch (Tag.attributes[curr].type) {
+    const $or = Object.keys(Order.attributes).reduce((acc, curr) => {
+      switch (Order.attributes[curr].type) {
         case 'integer':
         case 'float':
         case 'decimal':
@@ -188,7 +188,7 @@ module.exports = {
       }
     }, []);
 
-    return Tag
+    return Order
       .find({ $or })
       .sort(filters.sort)
       .skip(filters.start)
